4aed4c6b213345238992ae0fd3413b3123e57cbd
Gildas Quéméner <gildas.quemener@gmail.com>
2023-05-10 11:51:01 +0200

    make the rover move

--- src/MarsRover.php
+++ src/MarsRover.php
@@ -4,8 +4,17 @@ namespace GQ\Kata;
 
 class MarsRover
 {
+    private bool $moved = false;
+
     public function position(): string
     {
-        return "0:0:N";
+        return $this->moved ? '0:1:N' : '0:0:N';
+    }
+
+    public function execute(string $commands): void
+    {
+        if ($commands === 'M') {
+            $this->moved = true;
+        }
     }
 }
--- tests/MarsRoverTest.php
+++ tests/MarsRoverTest.php
@@ -14,4 +14,24 @@ final class MarsRoverTest extends TestCase
         $marsRover = new MarsRover();
         $this->assertEquals("0:0:N", $marsRover->position());
     }
+
+    #[Test]
+    public function should_not_change_position_when_executing_no_command(): void
+    {
+        $marsRover = new MarsRover();
+        $marsRover->execute('');
+
+        $this->assertEquals("0:0:N", $marsRover->position());
+    }
+
+    #[Test]
+    public function should_move_rover_forward_one_square_in_the_north_direction(): void
+    {
+        $marsRover = new MarsRover();
+        $marsRover->execute('M');
+
+        $this->assertEquals("0:1:N", $marsRover->position());
+    }
+
+    // TODO: should stay at non initial position when executing no command
 }
